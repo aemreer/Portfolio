d2c632c28f38d55f9c1a748b374726081a79a81c
Ahmet Emre Er <118760940+aemreer@users.noreply.github.com>
2025-11-20 17:40:51 +0300

    Add new project entry for Blog website demo

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -3,8 +3,8 @@ import ProjectCard from '../../components/ProjectCard'
 export default function ProjectsPage() {
     const projects = [
         { id: 1, title: "Furry", desc: "Furry e-ticaret demo", image: "/images/furry.png", link: "https://github.com/aemreer/furry-ecommerce" },
-
-        { id: 2, title: "Netflix Clone", desc: "Netflix klon demo", image: "/images/n.png", link: "https://github.com/aemreer/Netflix-Clone" }
+        { id: 2, title: "Netflix Clone", desc: "Netflix klon demo", image: "/images/n.png", link: "https://github.com/aemreer/Netflix-Clone" },
+        { id: 3, title: "Blog", desc: "Blog website demo", image: "/images/blog.png", link: "https://github.com/aemreer/Blog-Frontend" }
     ]
 
     return (
